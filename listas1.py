0c4898933574e804d0ab16dd0d1c2e9629c12b4f
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-05-16 13:37:40 -0300

    Add files via upload

---
+++ listas1.py
@@ -0,0 +1,10 @@
+temp = []
+temp.append(27)
+temp.append(30)
+temp.append(27.6)
+temp.append(23.5)
+temp.append(29.3)
+temp.append(24)
+temp.append(21)
+
+print(f"A menor temperatura identificada foi {min(temp)}°C e a maior temperatura foi {max(temp)}°C.")
