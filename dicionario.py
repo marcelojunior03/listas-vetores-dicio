2c80d04a71b4fae0e1cb0d5cb62e642475170569
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-05-17 13:56:25 -0300

    Atualizar o dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -10,4 +10,17 @@ while adicionar == 1:
     adicionar = int(input("Se deseja continuar, digite 1. Se deseja parar, digite 0: "))
 
 for i in range(0, len(listamusica)):
-    print(f"{listamusica[i]['nome']} : {listamusica[i]['função']} : {listamusica[i]['outros']}")
+    print(f"Cantora {i+1}: {listamusica[i]['nome']} : {listamusica[i]['função']} : {listamusica[i]['outros']}")
+
+remover = int(input("Se deseja remover alguma cantora da lista, digite 1. Caso queira mantê-la assim, digite 0: "))
+
+if remover == 1:
+    while remover == 1:
+        item = int(input("Certo, qual o número da cantora que deseja remover?\n"))
+        del listamusica[item - 1]
+        for i in range(0, len(listamusica)):
+            print(f"Nova lista:\nCantora {i+1}: {listamusica[i]['nome']} : {listamusica[i]['função']} : {listamusica[i]['outros']}")
+        remover = int(input("Se deseja remover outra cantora da lista, digite 1. Caso queira mantê-la assim, digite 0: "))
+
+if remover == 0:
+    print("Certo, muito obrigado!")
