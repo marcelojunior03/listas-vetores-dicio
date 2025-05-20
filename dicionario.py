01a263e9ffd5e84fb4f36dd33f2a8043e4065364
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-05-19 21:30:13 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -18,8 +18,9 @@ if remover == 1:
     while remover == 1:
         item = int(input("Certo, qual o número da cantora que deseja remover?\n"))
         del listamusica[item - 1]
+        print("Nova lista:")
         for i in range(0, len(listamusica)):
-            print(f"Nova lista:\nCantora {i+1}: {listamusica[i]['nome']} : {listamusica[i]['função']} : {listamusica[i]['outros']}")
+            print(f"Cantora {i+1}: {listamusica[i]['nome']} : {listamusica[i]['função']} : {listamusica[i]['outros']}")
         remover = int(input("Se deseja remover outra cantora da lista, digite 1. Caso queira mantê-la assim, digite 0: "))
 
 if remover == 0:
